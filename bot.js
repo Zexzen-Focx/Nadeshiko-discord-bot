4d33307026bb9506242e832fe2cd6ab614d01891
Zexzen-Focx <52243651+Zexzen-Focx@users.noreply.github.com>
2019-06-26 19:11:01 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,68 @@ client.on('ready', () => {
 	console.log('I am ready!');
 });
 
-
+//Bot Functions
+client.on('message', message => {
+	if(message.content.startsWith(config.prefix)){
+		const cmd = message.content.slice(1);
+		
+		if (cmd.startsWith('help')) {
+			message.channel.send('Help is work in progress, please be patient',{
+				reply: message.author
+			});
+		}
+		
+		else if(cmd.startsWith('boop')){
+			const user = message.mentions.users.first();
+			
+			const embed_msg = new RichEmbed()
+				.setTitle(data.imgs.boop.title)
+				.setImage(data.imgs.boop.url);
+			
+			if(user){
+				embed_msg.setDescription(user.username+' You have been booped by '+message.author.username.toString());
+				message.channel.send(embed_msg);
+			}else{
+				embed_msg.setDescription('You have been booped by '+message.author.username.toString());
+				message.channel.send(embed_msg);
+			}
+			
+		}
+		
+		if(cmd.startsWith('convert')){
+			var tempC;
+			var tempF;
+			var tempK;
+			var converted = false;
+			
+			if(message.content.endsWith('C')){
+				tempC = parseFloat(message.content.replace ( /[^\d.]/g, '' ));
+				tempF = (tempC*9/5)+32;
+				tempK = tempC+273.15;
+				converted = true;
+			}else if(message.content.endsWith('F')){
+				tempF = parseFloat(message.content.replace ( /[^\d.]/g, '' ));
+				tempC = (tempF-32)*5/9;
+				tempK = tempC+273.15;
+				converted = true;
+			}else if(message.content.endsWith('K')){
+				tempK = parseFloat(message.content.replace ( /[^\d.]/g, '' ));
+				tempC = tempK-273.15;
+				tempF = (tempC*9/5)+32;
+				converted = true;
+			}
+			
+			if(!converted){
+				message.channel.send('Gomen, I don\'t know that unit');
+			}else{
+				message.channel.send('Here are the conversion you asked:\n```'+tempC.toFixed(2)+' C\n'+tempF.toFixed(2)+' F\n'+tempK.toFixed(2)+' K```');
+			}
+		}
+		
+	}else{
+		
+	}
+	
+});
 
 client.login(process.env.BOT_TOKEN);
